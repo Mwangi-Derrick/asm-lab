e14270e603052f15c69dd4b37ad67a310119c4cc
Derrick Mwangi <mwangiderrick640@gmail.com>
2026-05-16 21:42:59 +0300

    revert to calling the hand tuned assembly

--- from-the-metal/03-simd-vectors/main.c
+++ from-the-metal/03-simd-vectors/main.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 
-// Implementing the logic in C to see how GCC vectorizes it
-void simd_add_c(double* res, double* a, double* b, int n) {
-    for (int i = 0; i < n; i++) {
-        res[i] = a[i] + b[i];
-    }
-}
+// This tells C that the function is defined elsewhere (in our .asm file)
+extern void simd_add(double* res, double* a, double* b, int n);
 
 int main() {
+    // We are processing 4 doubles (exactly one 256-bit YMM register)
     double a[4] = {1.5, 2.5, 3.5, 4.5};
     double b[4] = {10.0, 20.0, 30.0, 40.0};
     double res[4] = {0};
 
-    printf("Starting C-based SIMD addition (auto-vectorized)...\n");
+    printf("Starting hand-written SIMD addition (AVX)...\n");
 
-    simd_add_c(res, a, b, 4);
+    // Call our hand-written assembly function
+    simd_add(res, a, b, 4);
 
+    // Print results to verify
     for(int i = 0; i < 4; i++) {
         printf("res[%d]: %f + %f = %f\n", i, a[i], b[i], res[i]);
     }
