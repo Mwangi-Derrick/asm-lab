f70807729292f9802b22e3490e821a59f6ac7793
Derrick Mwangi <mwangiderrick640@gmail.com>
2026-05-16 21:26:42 +0300

    fear: print results

--- from-the-metal/03-simd-vectors/main.c
+++ from-the-metal/03-simd-vectors/main.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 
+// This tells C that the function is defined elsewhere (in our .asm file)
 extern void simd_add(double* res, double* a, double* b, int n);
 
 int main() {
-    double a[4] = {1.0, 2.0, 3.0, 4.0};
+    // We are processing 4 doubles (exactly one 256-bit YMM register)
+    double a[4] = {1.5, 2.5, 3.5, 4.5};
     double b[4] = {10.0, 20.0, 30.0, 40.0};
-    double res[4];
+    double res[4] = {0};
 
+    printf("Starting SIMD addition...\n");
+
+    // Call our hand-written assembly function
     simd_add(res, a, b, 4);
+
+    // Print results to verify
+    for(int i = 0; i < 4; i++) {
+        printf("res[%d]: %f + %f = %f\n", i, a[i], b[i], res[i]);
+    }
 
     return 0;
 }
