26c2fe0b9d929ee947cc6e3af1c6d68a5f2c9b59
Derrick Mwangi <mwangiderrick640@gmail.com>
2026-05-16 22:16:34 +0300

    updated

--- into-the-metal/rust/bounds.s
+++ into-the-metal/rust/bounds.s
@@ -5,15 +5,15 @@
 	.globl	@feat.00
 @feat.00 = 0
 	.file	"bounds.b313f5d2154a6840-cgu.0"
-	.def	_ZN3std2rt10lang_start17h22d2d52b53e54584E;
+	.def	_ZN3std2rt10lang_start17hee73d070ab074a6aE;
 	.scl	2;
 	.type	32;
 	.endef
-	.text
-	.globl	_ZN3std2rt10lang_start17h22d2d52b53e54584E
+	.section	.text,"xr",one_only,_ZN3std2rt10lang_start17hee73d070ab074a6aE,unique,0
+	.globl	_ZN3std2rt10lang_start17hee73d070ab074a6aE
 	.p2align	4
-_ZN3std2rt10lang_start17h22d2d52b53e54584E:
-.seh_proc _ZN3std2rt10lang_start17h22d2d52b53e54584E
+_ZN3std2rt10lang_start17hee73d070ab074a6aE:
+.seh_proc _ZN3std2rt10lang_start17hee73d070ab074a6aE
 	subq	$56, %rsp
 	.seh_stackalloc 56
 	.seh_endprologue
@@ -21,10 +21,10 @@ _ZN3std2rt10lang_start17h22d2d52b53e54584E:
 	movq	%rdx, %r8
 	movq	%rcx, 48(%rsp)
 	movb	%r9b, 32(%rsp)
-	leaq	.Lanon.89e5b14009693f1c21637f9a44849c3e.0(%rip), %rdx
+	leaq	anon.17b8263cc39aa9b1e9699867df80412a.0(%rip), %rdx
 	leaq	48(%rsp), %rcx
 	movq	%rax, %r9
-	callq	_ZN3std2rt19lang_start_internal17he4d198c5bc49b2b7E
+	callq	_ZN3std2rt19lang_start_internal17hf9be512cb4d4567aE
 	nop
 	.seh_startepilogue
 	addq	$56, %rsp
@@ -32,18 +32,19 @@ _ZN3std2rt10lang_start17h22d2d52b53e54584E:
 	retq
 	.seh_endproc
 
-	.def	_ZN3std2rt10lang_start28_$u7b$$u7b$closure$u7d$$u7d$17hf0037008a575b3d4E;
+	.def	_ZN3std2rt10lang_start28_$u7b$$u7b$closure$u7d$$u7d$17h5abbaa808f371fefE;
 	.scl	3;
 	.type	32;
 	.endef
+	.section	.text,"xr",one_only,_ZN3std2rt10lang_start28_$u7b$$u7b$closure$u7d$$u7d$17h5abbaa808f371fefE,unique,1
 	.p2align	4
-_ZN3std2rt10lang_start28_$u7b$$u7b$closure$u7d$$u7d$17hf0037008a575b3d4E:
-.seh_proc _ZN3std2rt10lang_start28_$u7b$$u7b$closure$u7d$$u7d$17hf0037008a575b3d4E
+_ZN3std2rt10lang_start28_$u7b$$u7b$closure$u7d$$u7d$17h5abbaa808f371fefE:
+.seh_proc _ZN3std2rt10lang_start28_$u7b$$u7b$closure$u7d$$u7d$17h5abbaa808f371fefE
 	subq	$40, %rsp
 	.seh_stackalloc 40
 	.seh_endprologue
 	movq	(%rcx), %rcx
-	callq	_ZN3std3sys9backtrace28__rust_begin_short_backtrace17hd31bfc2bd6a19d5eE
+	callq	_ZN3std3sys9backtrace28__rust_begin_short_backtrace17h7b14601821b9195eE
 	xorl	%eax, %eax
 	.seh_startepilogue
 	addq	$40, %rsp
@@ -51,13 +52,14 @@ _ZN3std2rt10lang_start28_$u7b$$u7b$closure$u7d$$u7d$17hf0037008a575b3d4E:
 	retq
 	.seh_endproc
 
-	.def	_ZN3std3sys9backtrace28__rust_begin_short_backtrace17hd31bfc2bd6a19d5eE;
+	.def	_ZN3std3sys9backtrace28__rust_begin_short_backtrace17h7b14601821b9195eE;
 	.scl	3;
 	.type	32;
 	.endef
+	.section	.text,"xr",one_only,_ZN3std3sys9backtrace28__rust_begin_short_backtrace17h7b14601821b9195eE,unique,2
 	.p2align	4
-_ZN3std3sys9backtrace28__rust_begin_short_backtrace17hd31bfc2bd6a19d5eE:
-.seh_proc _ZN3std3sys9backtrace28__rust_begin_short_backtrace17hd31bfc2bd6a19d5eE
+_ZN3std3sys9backtrace28__rust_begin_short_backtrace17h7b14601821b9195eE:
+.seh_proc _ZN3std3sys9backtrace28__rust_begin_short_backtrace17h7b14601821b9195eE
 	subq	$40, %rsp
 	.seh_stackalloc 40
 	.seh_endprologue
@@ -71,27 +73,29 @@ _ZN3std3sys9backtrace28__rust_begin_short_backtrace17hd31bfc2bd6a19d5eE:
 	retq
 	.seh_endproc
 
-	.def	_ZN42_$LT$$RF$T$u20$as$u20$core..fmt..Debug$GT$3fmt17hfdb0a624a010787aE;
+	.def	_ZN42_$LT$$RF$T$u20$as$u20$core..fmt..Debug$GT$3fmt17h877b28d2a3304d94E;
 	.scl	3;
 	.type	32;
 	.endef
+	.section	.text,"xr",one_only,_ZN42_$LT$$RF$T$u20$as$u20$core..fmt..Debug$GT$3fmt17h877b28d2a3304d94E,unique,3
 	.p2align	4
-_ZN42_$LT$$RF$T$u20$as$u20$core..fmt..Debug$GT$3fmt17hfdb0a624a010787aE:
+_ZN42_$LT$$RF$T$u20$as$u20$core..fmt..Debug$GT$3fmt17h877b28d2a3304d94E:
 	movq	(%rcx), %rcx
-	jmp	_ZN4core3fmt5float50_$LT$impl$u20$core..fmt..Debug$u20$for$u20$f64$GT$3fmt17h56c2885f79a3f110E
+	jmp	_ZN4core3fmt5float50_$LT$impl$u20$core..fmt..Debug$u20$for$u20$f64$GT$3fmt17h5349f0d13ba24d3cE
 
-	.def	_ZN4core3ops8function6FnOnce40call_once$u7b$$u7b$vtable.shim$u7d$$u7d$17hfd25b144931da2f3E;
+	.def	_ZN4core3ops8function6FnOnce40call_once$u7b$$u7b$vtable.shim$u7d$$u7d$17h748cf7d28a44554fE;
 	.scl	3;
 	.type	32;
 	.endef
+	.section	.text,"xr",one_only,_ZN4core3ops8function6FnOnce40call_once$u7b$$u7b$vtable.shim$u7d$$u7d$17h748cf7d28a44554fE,unique,4
 	.p2align	4
-_ZN4core3ops8function6FnOnce40call_once$u7b$$u7b$vtable.shim$u7d$$u7d$17hfd25b144931da2f3E:
-.seh_proc _ZN4core3ops8function6FnOnce40call_once$u7b$$u7b$vtable.shim$u7d$$u7d$17hfd25b144931da2f3E
+_ZN4core3ops8function6FnOnce40call_once$u7b$$u7b$vtable.shim$u7d$$u7d$17h748cf7d28a44554fE:
+.seh_proc _ZN4core3ops8function6FnOnce40call_once$u7b$$u7b$vtable.shim$u7d$$u7d$17h748cf7d28a44554fE
 	subq	$40, %rsp
 	.seh_stackalloc 40
 	.seh_endprologue
 	movq	(%rcx), %rcx
-	callq	_ZN3std3sys9backtrace28__rust_begin_short_backtrace17hd31bfc2bd6a19d5eE
+	callq	_ZN3std3sys9backtrace28__rust_begin_short_backtrace17h7b14601821b9195eE
 	xorl	%eax, %eax
 	.seh_startepilogue
 	addq	$40, %rsp
@@ -99,13 +103,14 @@ _ZN4core3ops8function6FnOnce40call_once$u7b$$u7b$vtable.shim$u7d$$u7d$17hfd25b14
 	retq
 	.seh_endproc
 
-	.def	_ZN4core5array69_$LT$impl$u20$core..fmt..Debug$u20$for$u20$$u5b$T$u3b$$u20$N$u5d$$GT$3fmt17h255ba4eb5850e262E;
+	.def	_ZN4core5array69_$LT$impl$u20$core..fmt..Debug$u20$for$u20$$u5b$T$u3b$$u20$N$u5d$$GT$3fmt17h7a57934db6e14d5bE;
 	.scl	3;
 	.type	32;
 	.endef
+	.section	.text,"xr",one_only,_ZN4core5array69_$LT$impl$u20$core..fmt..Debug$u20$for$u20$$u5b$T$u3b$$u20$N$u5d$$GT$3fmt17h7a57934db6e14d5bE,unique,5
 	.p2align	4
-_ZN4core5array69_$LT$impl$u20$core..fmt..Debug$u20$for$u20$$u5b$T$u3b$$u20$N$u5d$$GT$3fmt17h255ba4eb5850e262E:
-.seh_proc _ZN4core5array69_$LT$impl$u20$core..fmt..Debug$u20$for$u20$$u5b$T$u3b$$u20$N$u5d$$GT$3fmt17h255ba4eb5850e262E
+_ZN4core5array69_$LT$impl$u20$core..fmt..Debug$u20$for$u20$$u5b$T$u3b$$u20$N$u5d$$GT$3fmt17h7a57934db6e14d5bE:
+.seh_proc _ZN4core5array69_$LT$impl$u20$core..fmt..Debug$u20$for$u20$$u5b$T$u3b$$u20$N$u5d$$GT$3fmt17h7a57934db6e14d5bE
 	pushq	%r14
 	.seh_pushreg %r14
 	pushq	%rsi
@@ -120,34 +125,34 @@ _ZN4core5array69_$LT$impl$u20$core..fmt..Debug$u20$for$u20$$u5b$T$u3b$$u20$N$u5d
 	movq	%rcx, %rdi
 	leaq	40(%rsp), %rsi
 	movq	%rsi, %rcx
-	callq	_ZN4core3fmt9Formatter10debug_list17h542e43378537fbf2E
+	callq	_ZN4core3fmt9Formatter10debug_list17h018f8fcafcfd62efE
 	movq	%rdi, 32(%rsp)
-	leaq	.Lanon.89e5b14009693f1c21637f9a44849c3e.1(%rip), %rbx
+	leaq	anon.17b8263cc39aa9b1e9699867df80412a.1(%rip), %rbx
 	leaq	32(%rsp), %r14
 	movq	%rsi, %rcx
 	movq	%r14, %rdx
 	movq	%rbx, %r8
-	callq	_ZN4core3fmt8builders9DebugList5entry17h45796a47c12f6084E
+	callq	_ZN4core3fmt8builders9DebugList5entry17hf8c20daeff77a0d0E
 	leaq	8(%rdi), %rax
 	movq	%rax, 32(%rsp)
 	movq	%rsi, %rcx
 	movq	%r14, %rdx
 	movq	%rbx, %r8
-	callq	_ZN4core3fmt8builders9DebugList5entry17h45796a47c12f6084E
+	callq	_ZN4core3fmt8builders9DebugList5entry17hf8c20daeff77a0d0E
 	leaq	16(%rdi), %rax
 	movq	%rax, 32(%rsp)
 	movq	%rsi, %rcx
 	movq	%r14, %rdx
 	movq	%rbx, %r8
-	callq	_ZN4core3fmt8builders9DebugList5entry17h45796a47c12f6084E
+	callq	_ZN4core3fmt8builders9DebugList5entry17hf8c20daeff77a0d0E
 	addq	$24, %rdi
 	movq	%rdi, 32(%rsp)
 	movq	%rsi, %rcx
 	movq	%r14, %rdx
 	movq	%rbx, %r8
-	callq	_ZN4core3fmt8builders9DebugList5entry17h45796a47c12f6084E
+	callq	_ZN4core3fmt8builders9DebugList5entry17hf8c20daeff77a0d0E
 	movq	%rsi, %rcx
-	callq	_ZN4core3fmt8builders9DebugList6finish17h437518fb0a9af433E
+	callq	_ZN4core3fmt8builders9DebugList6finish17hbd5050d1522c2deeE
 	nop
 	.seh_startepilogue
 	addq	$56, %rsp
@@ -163,21 +168,31 @@ _ZN4core5array69_$LT$impl$u20$core..fmt..Debug$u20$for$u20$$u5b$T$u3b$$u20$N$u5d
 	.scl	2;
 	.type	32;
 	.endef
-	.section	.rdata,"dr"
+	.globl	__xmm@40000000000000003ff0000000000000
+	.section	.rdata,"dr",discard,__xmm@40000000000000003ff0000000000000
 	.p2align	4, 0x0
-.LCPI6_0:
+__xmm@40000000000000003ff0000000000000:
 	.quad	0x3ff0000000000000
 	.quad	0x4000000000000000
-.LCPI6_1:
+	.globl	__xmm@40100000000000004008000000000000
+	.section	.rdata,"dr",discard,__xmm@40100000000000004008000000000000
+	.p2align	4, 0x0
+__xmm@40100000000000004008000000000000:
 	.quad	0x4008000000000000
 	.quad	0x4010000000000000
-.LCPI6_2:
+	.globl	__xmm@40340000000000004024000000000000
+	.section	.rdata,"dr",discard,__xmm@40340000000000004024000000000000
+	.p2align	4, 0x0
+__xmm@40340000000000004024000000000000:
 	.quad	0x4024000000000000
 	.quad	0x4034000000000000
-.LCPI6_3:
+	.globl	__xmm@4044000000000000403e000000000000
+	.section	.rdata,"dr",discard,__xmm@4044000000000000403e000000000000
+	.p2align	4, 0x0
+__xmm@4044000000000000403e000000000000:
 	.quad	0x403e000000000000
 	.quad	0x4044000000000000
-	.text
+	.section	.text,"xr",one_only,_ZN6bounds4main17h1503e6a39176505dE,unique,6
 	.globl	_ZN6bounds4main17h1503e6a39176505dE
 	.p2align	4
 _ZN6bounds4main17h1503e6a39176505dE:
@@ -187,13 +202,13 @@ _ZN6bounds4main17h1503e6a39176505dE:
 	subq	$160, %rsp
 	.seh_stackalloc 160
 	.seh_endprologue
-	movaps	.LCPI6_0(%rip), %xmm0
+	movaps	__xmm@40000000000000003ff0000000000000(%rip), %xmm0
 	movaps	%xmm0, 64(%rsp)
-	movaps	.LCPI6_1(%rip), %xmm0
+	movaps	__xmm@40100000000000004008000000000000(%rip), %xmm0
 	movaps	%xmm0, 80(%rsp)
-	movaps	.LCPI6_2(%rip), %xmm0
+	movaps	__xmm@40340000000000004024000000000000(%rip), %xmm0
 	movaps	%xmm0, 96(%rsp)
-	movaps	.LCPI6_3(%rip), %xmm0
+	movaps	__xmm@4044000000000000403e000000000000(%rip), %xmm0
 	movaps	%xmm0, 112(%rsp)
 	xorps	%xmm0, %xmm0
 	movaps	%xmm0, 144(%rsp)
@@ -207,11 +222,11 @@ _ZN6bounds4main17h1503e6a39176505dE:
 	movl	$4, %r9d
 	callq	add_arrays
 	movq	%rsi, 48(%rsp)
-	leaq	_ZN4core5array69_$LT$impl$u20$core..fmt..Debug$u20$for$u20$$u5b$T$u3b$$u20$N$u5d$$GT$3fmt17h255ba4eb5850e262E(%rip), %rax
+	leaq	_ZN4core5array69_$LT$impl$u20$core..fmt..Debug$u20$for$u20$$u5b$T$u3b$$u20$N$u5d$$GT$3fmt17h7a57934db6e14d5bE(%rip), %rax
 	movq	%rax, 56(%rsp)
-	leaq	.Lanon.89e5b14009693f1c21637f9a44849c3e.2(%rip), %rcx
+	leaq	anon.17b8263cc39aa9b1e9699867df80412a.2(%rip), %rcx
 	leaq	48(%rsp), %rdx
-	callq	_ZN3std2io5stdio6_print17h1a35dc73fb86a1adE
+	callq	_ZN3std2io5stdio6_print17h31aa9321f320e7a8E
 	nop
 	.seh_startepilogue
 	addq	$160, %rsp
@@ -224,6 +239,7 @@ _ZN6bounds4main17h1503e6a39176505dE:
 	.scl	2;
 	.type	32;
 	.endef
+	.section	.text,"xr",one_only,add_arrays,unique,7
 	.globl	add_arrays
 	.p2align	4
 add_arrays:
@@ -301,14 +317,14 @@ add_arrays:
 	.seh_endepilogue
 	retq
 .LBB7_10:
-	leaq	.Lanon.89e5b14009693f1c21637f9a44849c3e.4(%rip), %r8
+	leaq	anon.17b8263cc39aa9b1e9699867df80412a.4(%rip), %r8
 	movq	%r9, %rcx
 	movq	%r9, %rdx
-	callq	_ZN4core9panicking18panic_bounds_check17hc208fef1f4bcd83fE
+	callq	_ZN4core9panicking18panic_bounds_check17ha95d63c16a71bfedE
 .LBB7_11:
-	leaq	.Lanon.89e5b14009693f1c21637f9a44849c3e.5(%rip), %r8
+	leaq	anon.17b8263cc39aa9b1e9699867df80412a.5(%rip), %r8
 	movq	%rcx, %rdx
-	callq	_ZN4core9panicking18panic_bounds_check17hc208fef1f4bcd83fE
+	callq	_ZN4core9panicking18panic_bounds_check17ha95d63c16a71bfedE
 	int3
 	.seh_endproc
 
@@ -316,71 +332,61 @@ add_arrays:
 	.scl	2;
 	.type	32;
 	.endef
+	.section	.text,"xr",one_only,main,unique,8
 	.globl	main
 	.p2align	4
 main:
 .seh_proc main
-	pushq	%rbp
-	.seh_pushreg %rbp
-	pushq	%rsi
-	.seh_pushreg %rsi
-	pushq	%rdi
-	.seh_pushreg %rdi
-	subq	$48, %rsp
-	.seh_stackalloc 48
-	leaq	48(%rsp), %rbp
-	.seh_setframe %rbp, 48
+	subq	$56, %rsp
+	.seh_stackalloc 56
 	.seh_endprologue
-	movq	%rdx, %rsi
-	movslq	%ecx, %rdi
-	callq	__main
+	movq	%rdx, %r9
+	movslq	%ecx, %r8
 	leaq	_ZN6bounds4main17h1503e6a39176505dE(%rip), %rax
-	movq	%rax, -8(%rbp)
+	movq	%rax, 48(%rsp)
 	movb	$0, 32(%rsp)
-	leaq	.Lanon.89e5b14009693f1c21637f9a44849c3e.0(%rip), %rdx
-	leaq	-8(%rbp), %rcx
-	movq	%rdi, %r8
-	movq	%rsi, %r9
-	callq	_ZN3std2rt19lang_start_internal17he4d198c5bc49b2b7E
+	leaq	anon.17b8263cc39aa9b1e9699867df80412a.0(%rip), %rdx
+	leaq	48(%rsp), %rcx
+	callq	_ZN3std2rt19lang_start_internal17hf9be512cb4d4567aE
 	nop
 	.seh_startepilogue
-	addq	$48, %rsp
-	popq	%rdi
-	popq	%rsi
-	popq	%rbp
+	addq	$56, %rsp
 	.seh_endepilogue
 	retq
 	.seh_endproc
 
-	.section	.rdata,"dr"
+	.section	.rdata,"dr",one_only,anon.17b8263cc39aa9b1e9699867df80412a.0,unique,9
 	.p2align	3, 0x0
-.Lanon.89e5b14009693f1c21637f9a44849c3e.0:
+anon.17b8263cc39aa9b1e9699867df80412a.0:
 	.asciz	"\000\000\000\000\000\000\000\000\b\000\000\000\000\000\000\000\b\000\000\000\000\000\000"
-	.quad	_ZN4core3ops8function6FnOnce40call_once$u7b$$u7b$vtable.shim$u7d$$u7d$17hfd25b144931da2f3E
-	.quad	_ZN3std2rt10lang_start28_$u7b$$u7b$closure$u7d$$u7d$17hf0037008a575b3d4E
-	.quad	_ZN3std2rt10lang_start28_$u7b$$u7b$closure$u7d$$u7d$17hf0037008a575b3d4E
+	.quad	_ZN4core3ops8function6FnOnce40call_once$u7b$$u7b$vtable.shim$u7d$$u7d$17h748cf7d28a44554fE
+	.quad	_ZN3std2rt10lang_start28_$u7b$$u7b$closure$u7d$$u7d$17h5abbaa808f371fefE
+	.quad	_ZN3std2rt10lang_start28_$u7b$$u7b$closure$u7d$$u7d$17h5abbaa808f371fefE
 
+	.section	.rdata,"dr",one_only,anon.17b8263cc39aa9b1e9699867df80412a.1,unique,10
 	.p2align	3, 0x0
-.Lanon.89e5b14009693f1c21637f9a44849c3e.1:
+anon.17b8263cc39aa9b1e9699867df80412a.1:
 	.asciz	"\000\000\000\000\000\000\000\000\b\000\000\000\000\000\000\000\b\000\000\000\000\000\000"
-	.quad	_ZN42_$LT$$RF$T$u20$as$u20$core..fmt..Debug$GT$3fmt17hfdb0a624a010787aE
+	.quad	_ZN42_$LT$$RF$T$u20$as$u20$core..fmt..Debug$GT$3fmt17h877b28d2a3304d94E
 
-.Lanon.89e5b14009693f1c21637f9a44849c3e.2:
+	.section	.rdata,"dr",one_only,anon.17b8263cc39aa9b1e9699867df80412a.2,unique,11
+anon.17b8263cc39aa9b1e9699867df80412a.2:
 	.asciz	"\300\001\n"
 
-.Lanon.89e5b14009693f1c21637f9a44849c3e.3:
+	.section	.rdata,"dr",one_only,anon.17b8263cc39aa9b1e9699867df80412a.3,unique,12
+anon.17b8263cc39aa9b1e9699867df80412a.3:
 	.asciz	"into-the-metal/rust/bounds.rs"
 
+	.section	.rdata,"dr",one_only,anon.17b8263cc39aa9b1e9699867df80412a.4,unique,13
 	.p2align	3, 0x0
-.Lanon.89e5b14009693f1c21637f9a44849c3e.4:
-	.quad	.Lanon.89e5b14009693f1c21637f9a44849c3e.3
+anon.17b8263cc39aa9b1e9699867df80412a.4:
+	.quad	anon.17b8263cc39aa9b1e9699867df80412a.3
 	.asciz	"\035\000\000\000\000\000\000\000\005\000\000\000\031\000\000"
 
+	.section	.rdata,"dr",one_only,anon.17b8263cc39aa9b1e9699867df80412a.5,unique,14
 	.p2align	3, 0x0
-.Lanon.89e5b14009693f1c21637f9a44849c3e.5:
-	.quad	.Lanon.89e5b14009693f1c21637f9a44849c3e.3
+anon.17b8263cc39aa9b1e9699867df80412a.5:
+	.quad	anon.17b8263cc39aa9b1e9699867df80412a.3
 	.asciz	"\035\000\000\000\000\000\000\000\005\000\000\000\t\000\000"
 
-	.section	.drectve,"yni"
-	.ascii	" -exclude-symbols:_ZN3std2rt10lang_start17h22d2d52b53e54584E"
-	.ascii	" -exclude-symbols:_ZN6bounds4main17h1503e6a39176505dE"
+	.globl	_fltused
