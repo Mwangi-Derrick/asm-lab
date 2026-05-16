1439451e4f09793dde754bff6e659919dd816c15
Derrick Mwangi <mwangiderrick640@gmail.com>
2026-05-16 21:52:57 +0300

    feat: implemented vector additions, go:inline and no_mangle compiler flags ensure the function names arent mangled in binary

--- into-the-metal/go/stack.go
+++ into-the-metal/go/stack.go
@@ -2,10 +2,18 @@ package main
 
 import "fmt"
 
+//go:noinline
+func addSlices(a, b, res []float64) {
+	for i := range a {
+		res[i] = a[i] + b[i]
+	}
+}
+
 func main() {
-    a := make([]int, 10)
-    for i := range a {
-        a[i] = i * i
-    }
-    fmt.Println(a)
+	a := []float64{1.0, 2.0, 3.0, 4.0}
+	b := []float64{10.0, 20.0, 30.0, 40.0}
+	res := make([]float64, 4)
+
+	addSlices(a, b, res)
+	fmt.Println(res)
 }
--- into-the-metal/rust/bounds.rs
+++ into-the-metal/rust/bounds.rs
@@ -1,12 +1,16 @@
-pub fn sum_array(arr: &[u32]) -> u32 {
-    let mut sum = 0;
-    for &val in arr {
-        sum += val;
+#[no_mangle]
+#[inline(never)]
+pub fn add_arrays(a: &[f64], b: &[f64], res: &mut [f64]) {
+    for i in 0..a.len() {
+        res[i] = a[i] + b[i];
     }
-    sum
 }
 
 fn main() {
-    let arr = [1, 2, 3, 4, 5];
-    println!("Sum: {}", sum_array(&arr));
+    let a = [1.0, 2.0, 3.0, 4.0];
+    let b = [10.0, 20.0, 30.0, 40.0];
+    let mut res = [0.0; 4];
+
+    add_arrays(&a, &b, &mut res);
+    println!("{:?}", res);
 }
