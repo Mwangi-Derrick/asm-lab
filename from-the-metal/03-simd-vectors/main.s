eff93ca9a57f126526d51a7edc4a09a4f88f6ee3
Derrick Mwangi <mwangiderrick640@gmail.com>
2026-05-16 21:42:16 +0300

    vectorized

--- from-the-metal/03-simd-vectors/main.s
+++ from-the-metal/03-simd-vectors/main.s
@@ -1,10 +1,103 @@
 	.file	"main.c"
 	.intel_syntax noprefix
 	.text
+	.p2align 4
+	.globl	simd_add_c
+	.def	simd_add_c;	.scl	2;	.type	32;	.endef
+	.seh_proc	simd_add_c
+simd_add_c:
+	.seh_endprologue
+	test	r9d, r9d
+	jle	.L31
+	cmp	r9d, 1
+	je	.L3
+	lea	rax, -8[rcx]
+	mov	r10, rax
+	sub	r10, rdx
+	cmp	r10, 16
+	jbe	.L3
+	sub	rax, r8
+	cmp	rax, 16
+	jbe	.L3
+	lea	eax, -1[r9]
+	mov	r11d, r9d
+	cmp	eax, 2
+	jbe	.L11
+	mov	r10d, r9d
+	xor	eax, eax
+	shr	r10d, 2
+	sal	r10, 5
+	.p2align 5
+	.p2align 4
+	.p2align 3
+.L5:
+	vmovupd	ymm0, YMMWORD PTR [r8+rax]
+	vaddpd	ymm0, ymm0, YMMWORD PTR [rdx+rax]
+	vmovupd	YMMWORD PTR [rcx+rax], ymm0
+	add	rax, 32
+	cmp	rax, r10
+	jne	.L5
+	mov	eax, r9d
+	and	eax, -4
+	mov	r10d, eax
+	cmp	r9d, eax
+	je	.L29
+	mov	r11d, r9d
+	sub	r11d, eax
+	cmp	r11d, 1
+	je	.L33
+	vzeroupper
+.L4:
+	mov	r9d, r10d
+	vmovupd	xmm0, XMMWORD PTR [r8+r9*8]
+	vaddpd	xmm0, xmm0, XMMWORD PTR [rdx+r9*8]
+	vmovupd	XMMWORD PTR [rcx+r9*8], xmm0
+	test	r11b, 1
+	je	.L31
+	and	r11d, -2
+	add	eax, r11d
+.L7:
+	cdqe
+	vmovsd	xmm0, QWORD PTR [rdx+rax*8]
+	vaddsd	xmm0, xmm0, QWORD PTR [r8+rax*8]
+	vmovsd	QWORD PTR [rcx+rax*8], xmm0
+.L31:
+	ret
+	.p2align 4,,10
+	.p2align 3
+.L29:
+	vzeroupper
+	ret
+	.p2align 4,,10
+	.p2align 3
+.L3:
+	movsx	r9, r9d
+	xor	eax, eax
+	sal	r9, 3
+	.p2align 5
+	.p2align 4
+	.p2align 3
+.L9:
+	vmovsd	xmm0, QWORD PTR [rdx+rax]
+	vaddsd	xmm0, xmm0, QWORD PTR [r8+rax]
+	vmovsd	QWORD PTR [rcx+rax], xmm0
+	add	rax, 8
+	cmp	r9, rax
+	jne	.L9
+	ret
+.L11:
+	xor	r10d, r10d
+	xor	eax, eax
+	jmp	.L4
+.L33:
+	vzeroupper
+	jmp	.L7
+	.seh_endproc
 	.section .rdata,"dr"
+	.align 8
 .LC2:
-	.ascii "Starting SIMD addition...\0"
-.LC3:
+	.ascii "Starting C-based SIMD addition (auto-vectorized)...\0"
+.LC4:
 	.ascii "res[%d]: %f + %f = %f\12\0"
 	.section	.text.startup,"x"
 	.p2align 4
@@ -32,21 +125,17 @@ main:
 	vmovupd	YMMWORD PTR 48[rsp], ymm0
 	vmovupd	ymm0, YMMWORD PTR .LC1[rip]
 	vmovupd	YMMWORD PTR 80[rsp], ymm0
-	vpxor	xmm0, xmm0, xmm0
-	vmovdqu	YMMWORD PTR 112[rsp], ymm0
 	vzeroupper
 	call	puts
-	mov	r8, rbp
-	mov	rdx, rdi
-	mov	r9d, 4
-	mov	rcx, rsi
+	vmovupd	ymm0, YMMWORD PTR .LC3[rip]
 	xor	ebx, ebx
-	call	simd_add
-.L2:
+	vmovupd	YMMWORD PTR 112[rsp], ymm0
+	vzeroupper
+.L35:
 	mov	r9, QWORD PTR 0[rbp+rbx*8]
 	mov	r8, QWORD PTR [rdi+rbx*8]
 	mov	edx, ebx
-	lea	rcx, .LC3[rip]
+	lea	rcx, .LC4[rip]
 	vmovsd	xmm0, QWORD PTR [rsi+rbx*8]
 	add	rbx, 1
 	vmovq	xmm3, r9
@@ -54,7 +143,7 @@ main:
 	vmovsd	QWORD PTR 32[rsp], xmm0
 	call	printf
 	cmp	rbx, 4
-	jne	.L2
+	jne	.L35
 	xor	eax, eax
 	add	rsp, 152
 	pop	rbx
@@ -84,8 +173,17 @@ main:
 	.long	1077805056
 	.long	0
 	.long	1078198272
+	.align 32
+.LC3:
+	.long	0
+	.long	1076297728
+	.long	0
+	.long	1077313536
+	.long	0
+	.long	1077985280
+	.long	0
+	.long	1078345728
 	.def	__main;	.scl	2;	.type	32;	.endef
 	.ident	"GCC: (Rev8, Built by MSYS2 project) 15.2.0"
 	.def	puts;	.scl	2;	.type	32;	.endef
-	.def	simd_add;	.scl	2;	.type	32;	.endef
 	.def	printf;	.scl	2;	.type	32;	.endef
