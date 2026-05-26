32ca1319e46c0da15827f5c701d08fb908e5d015
Derrick Mwangi <mwangiderrick640@gmail.com>
2026-05-26 14:46:28 +0300

    chore: generated asm following intel syntax

--- into-the-metal/c-cpp/add.asm
+++ into-the-metal/c-cpp/add.asm
@@ -1,21 +1,18 @@
 	.file	"add.c"
+	.intel_syntax noprefix
 	.text
+	.section	.text.startup,"x"
+	.p2align 4
 	.globl	main
 	.def	main;	.scl	2;	.type	32;	.endef
 	.seh_proc	main
 main:
-	pushq	%rbp
-	.seh_pushreg	%rbp
-	movq	%rsp, %rbp
-	.seh_setframe	%rbp, 0
-	subq	$48, %rsp
-	.seh_stackalloc	48
+	sub	rsp, 40
+	.seh_stackalloc	40
 	.seh_endprologue
 	call	__main
-	movl	$150, -4(%rbp)
-	movl	-4(%rbp), %eax
-	addq	$48, %rsp
-	popq	%rbp
+	mov	eax, 150
+	add	rsp, 40
 	ret
 	.seh_endproc
 	.def	__main;	.scl	2;	.type	32;	.endef
