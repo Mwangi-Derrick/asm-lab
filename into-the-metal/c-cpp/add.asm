c97dfbbed8b4e80c0f089da1a5e37bdb93d41645
Derrick Mwangi <mwangiderrick640@gmail.com>
2026-05-26 14:43:52 +0300

    x86-64 asm instructions for add

---
+++ into-the-metal/c-cpp/add.asm
@@ -0,0 +1,22 @@
+	.file	"add.c"
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$48, %rsp
+	.seh_stackalloc	48
+	.seh_endprologue
+	call	__main
+	movl	$150, -4(%rbp)
+	movl	-4(%rbp), %eax
+	addq	$48, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.ident	"GCC: (Rev8, Built by MSYS2 project) 15.2.0"
